deb5a74cb8924e967ff758b616aac68bd3d192be
Aadam Mazema <maze.aadam@gmail.com>
2025-02-07 14:36:43 +0200

    Added a you section checking if the user is returning and a function that handles it

--- src/index.ts
+++ src/index.ts
@@ -25,9 +25,29 @@ const reviews = [
   },
 ];
 
-function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
-  const iconDisplay = isLoyalty ? '⭐' : ''
-  reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + iconDisplay
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+  const iconDisplay = isLoyalty ? "⭐" : "";
+  reviewTotalDisplay.innerHTML =
+    "review total " +
+    value.toString() +
+    "| last reviewed by " +
+    reviewer +
+    iconDisplay;
 }
 
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+const you = {
+  userName: 'Bobby',
+  isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+  if (isReturning){
+      returningUserDisplay.innerHTML = 'back'
+  }
+  userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
